a328822c625e7dbe7955c9aaec006c4636f47ba7
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-04-27 01:29:49 -0700

    final static todos

--- server.js
+++ server.js
@@ -56,10 +56,21 @@ app.get('/todos', function (req,res) {
          }
 
 
+
    // if has property && complete=true 
    // filteredTodos = _.where(filteredTodos,?)
    // ele if hs prop && completed if 'false'
 
+   if (queryParams.hasOwnProperty('q') && queryParams.q.length >0) {
+
+   filteredTodos = _.filter( filteredTodos , function (todo) {
+    return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1 ; 
+
+   } );
+
+   }
+
+// 'Go to work on Saturday'.indexOf('work') 
 
 
 	res.json(filteredTodos); 

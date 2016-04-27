d70b38afe154655b74570c2656744768125f1996
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-04-26 17:25:34 -0700

    refactor API with underscore

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 
+
+
 var express = require('express') ;
 
 var bodyParser = require('body-parser');
 // this is middleware...i dont get it
-
+var _ = require('underscore');
 
 var app= express();
+
 var PORT= process.env.PORT || 3000 ; 
+// where does this server run at???
 
 var todos= [ ];
 
@@ -14,6 +18,8 @@ var todoNextID = 1
 
 
 app.use(bodyParser.json());
+// have no idea... what is this thing even doing?
+
 
 
 //Model = The Todo object
@@ -23,34 +29,50 @@ app.use(bodyParser.json());
 app.get('/' ,  function (req, res) {
 	res.send('Todo API Root');
 
-});
+}); 
+
+// this code will only take as a res an empty page with todoAPI ROOT
+
+
+//Q: whats the differnce between res.send and res.json ????
 //Requests:
-// GET /todos    > to get all of the todo Items
+// GET /todos    > to get all of the todo Items 
+
+
 app.get('/todos', function (req,res) {
 	res.json(todos); // to convert to JSON
 	// we can use POSTman to test if the get request 
 	//is working.
 } )
+
+
 //GET /todos/:id > to get individual 
 
 app.get('/todos/:id', function (req,res) {
 
-	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo;
+
+	var todoId = parseInt(req.params.id, 10); // this will make 
+	//the code underneath work, because todoID needs to be a number 
+	//within the array?? 
+	
+var matchedTodo= _.findWhere(todos, {id: todoId}); 
+//findwhere will take 2 argumets , the array and what youre looking for
+//the code above is using underscore for the search
 
 
+//var matchedTodo;
+    // todos.forEach( function (todo){
+    // 	  if(todoId === todo.id ){
+    // 	  	matchedTodo =  todo;
+    // 	  }
+    // 	});    
+//  the commented above is only by using normal js code
 
 
-    todos.forEach( function (todo){
-    	  if(todoId === todo.id ){
-    	  	matchedTodo =  todo;
-    	  }
-
-
-    	});
 
  if (matchedTodo) {
  	   res.json(matchedTodo);
+
  } else {
 
  res.status(404).send();
@@ -61,19 +83,36 @@ app.get('/todos/:id', function (req,res) {
 
 })
 
+
+
+
+// In the Code below, we will use app.post to take an input from the user
+// by using the postman req.body 
  
  // POST/todos request , new http method it can take data,
+
+
 app.post('/todos' , function (req,res){
 
-	
-	var body = req.body;
+
+	var body = _.pick(req.body , 'description' , 'completed');// okay so req.body to require the body which is 
+	//anything inputed into the postman body. 
+
+	if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0 ){
+		return res.status(400).send();
+		//400 means bad data was provided 
+	}
+
+	body.description = body.description.trim();
+
 
          body.id= todoNextID++ ;
 
 		todos.push(body);
 
 
-	res.json(body)
+	res.json(body) // i guess this is so that at the end the page will
+	//show the body. 
 
 })
 

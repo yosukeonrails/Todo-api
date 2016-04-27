66eff93fc85e2538218ef8a191e23f0f049d1da7
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-04-26 23:48:29 -0700

    Add PUT todos/

--- server.js
+++ server.js
@@ -135,6 +135,42 @@ app.post('/todos' , function (req,res){
 
    })
 
+   //PUT / todos/:id 
+   app.put('/todos/:id' , function (req, res){
+
+  var todoId = parseInt(req.params.id, 10); 
+  var matchedTodo= _.findWhere(todos, {id: todoId}); 
+
+   	var body = _.pick(req.body , 'description' , 'completed')
+   	var validAttributes= {};
+
+
+   	if(!matchedTodo) {return res.status(404).send();}
+
+
+
+   	if (body.hasOwnProperty('completed') && _.isBoolean(body.completed)) {
+   		validAttributes.completed = body.completed;
+   	}  else if (body.hasOwnProperty('completed')) {
+   		return res.status(400).send(); 
+   	} 
+
+   	if (body.hasOwnProperty('description') && _.isString(body.description) &&
+   		body.description.trim().length > 0 ) {
+   		validAttributes.description = body.description
+   	} else if (body.hasOwnProperty('description')) {
+   		return res.status(400).send(); 	
+   	} 
+
+   _.extend(matchedTodo , validAttributes);
+
+res.json(matchedTodo);
+   		//	body.hasOwnProperty('completed')
+
+   		//copies one object to another
+
+   })
+
 
 app.listen(PORT , function () {
 	console.log('Express listening on ' + PORT +  ' ! ');

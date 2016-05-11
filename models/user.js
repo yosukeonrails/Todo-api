14f32e9d831575b19e56db985ad4eb0eb19c1bce
Yosuke Hishinuma <yosukeonrails@gmail.com>
2016-05-10 18:00:21 -0700

    neither  modified after

--- models/user.js
+++ models/user.js
@@ -1,76 +1,86 @@
 var bcrypt = require('bcrypt');
-var _ = require('underscore');
+	var _ = require ('underscore');
 
-module.exports = function(sequelize, DataTypes) {
-	var user = sequelize.define('user', {
-		email: {
-			type: DataTypes.STRING,
-			allowNull: false,
-			unique: true,
-			validate: {
-				isEmail: true
-			}
-		},
-		salt: {
-			type: DataTypes.STRING
-		},
-		password_hash: {
-			type: DataTypes.STRING
-		},
-		password: {
-			type: DataTypes.VIRTUAL,
-			allowNull: false,
-			validate: {
-				len: [7, 100]
-			},
-			set: function(value) {
-				var salt = bcrypt.genSaltSync(10);
-				var hashedPassword = bcrypt.hashSync(value, salt);
+		module.exports = function (sequelize , DataTypes) {
+			var user= sequelize.define('user', {
+			email: {
+				type: DataTypes.STRING,
+				allowNull: false,
+				unique: true,
+				validate:{
+					isEmail: true
+				     } 
+				},
+				salt: {
+					type: DataTypes.STRING
+				},
+				password_hash: {
+					type: DataTypes.STRING
+				},
+                password: {
+					type: DataTypes.VIRTUAL,
+					allowNull: false,
+					validate: {
+						len: [7 , 100]
+					},
+					set: function (value) {
+						var salt = bcrypt.genSaltSync(10);
+						var hashedPassword = bcrypt.hashSync(value, salt);
 
-				this.setDataValue('password', value);
-				this.setDataValue('salt', salt);
-				this.setDataValue('password_hash', hashedPassword);
-			}
-		}
-	}, {
-		hooks: {
-			beforeValidate: function(user, options) {
-				// user.email
-				if (typeof user.email === 'string') {
-					user.email = user.email.toLowerCase();
-				}
-			}
-		},
-		classMethods: {
-			authenticate: function(body) {
-				return new Promise(function(resolve, reject) {
-					if (typeof body.email !== 'string' || typeof body.password !== 'string') {
-						return reject();
+						this.setDataValue('password', value);
+						this.setDataValue('salt' , salt);
+						this.setDataValue ('password_hash' , hashedPassword);
 					}
+				}
 
-					user.findOne({
-						where: {
-							email: body.email
-						}
-					}).then(function(user) {
-						if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-							return reject();
-						}
+			},  {
+			 hooks: {
+	     	beforeValidate: function (user , options) {
 
-						resolve(user);
-					}, function(e) {
-						reject();
-					});
-				});
-			}
-		},
-		instanceMethods: {
-			toPublicJSON: function() {
-				var json = this.toJSON();
-				return _.pick(json, 'id', 'email', 'createdAt', 'updatedAt');
-			}
-		}
-	});
+                  if (typeof user.email === 'string') {
+                  	user.email =  user.email.toLowerCase();
+                  	  	}
+				     }
+				 },
+				 classMethods:{
+				 	authenticate: function (body) {
+				 		return new Promise(function(resolve, reject){
+				 	  if( typeof body.email !== 'string' || typeof body.password !== 'string') {
+				 	  	return reject() ;
 
-	return user;
-};
+				 	  }
+
+				      user.findOne({ 
+				      where: {
+				      email: body.email
+				     }
+				   }).then(function (user) {
+				       if (!user || ! bcrypt.compareSync(body.password, user.get('password_hash'))) {
+				        return reject();
+				       }
+
+				       resolve(user);
+
+				   }, function (e) {
+				          reject();
+
+				 
+ 				      });
+
+				 		});
+
+				 	}
+
+				 },
+				 instanceMethods: {
+				 	toPublicJSON: function () {
+				 		var json = this.toJSON();
+				 		return _.pick(json, 'id' , 'email' , 'createdAt' , 'updatedAt');
+				 	}
+				 }
+
+		});
+
+       return user;
+
+		};
